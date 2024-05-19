60b7b31c08cae410a681a2116d37ed5aeb6c9bea
Fabian Becker <info@alertrix.net>
2024-05-20 00:01:02 +0200

    add urlpatterns for alertrix

--- src/alertrix_deployment/urls.py
+++ src/alertrix_deployment/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('alertrix.urls')),
     path('_matrix/app/', include('matrixappservice.urls')),
 ]

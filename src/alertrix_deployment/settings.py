4be59a70242fe3a923ed8b0a75691504ed774170
Fabian Becker <info@alertrix.net>
2024-05-19 15:25:10 +0200

    import: gettext_lazy as _ from django.utils.translation into settings.py

--- src/alertrix_deployment/settings.py
+++ src/alertrix_deployment/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

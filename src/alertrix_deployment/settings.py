553dbb14081953b55439a1478d69f2f6cd7506ac
Fabian Becker <info@alertrix.net>
2024-08-06 23:48:24 +0200

    add a context processor which adds settings.SERVICE_NAME to all templates

--- src/alertrix_deployment/settings.py
+++ src/alertrix_deployment/settings.py
@@ -129,6 +129,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'alertrix.context_processors.service_name',
             ],
         },
     },

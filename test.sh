72022434fe8cfb7925526fe479cbda5ec206e135
Fabian Becker <info@alertrix.net>
2024-09-17 11:25:07 +0200

    add nginx config check to test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-sudo docker compose -f compose-test.yml build && sudo docker compose -f compose-test.yml down --volumes --remove-orphans && sudo docker compose -f compose-test.yml up -d && sudo docker compose -f compose-test.yml logs -f alertrix && rm test/data/homeserver.db
+sudo docker compose -f compose-test.yml build && sudo docker compose -f compose-test.yml down --volumes --remove-orphans && sudo docker compose -f compose-test.yml up -d && sudo docker compose exec nginx nginx -t && sudo docker compose -f compose-test.yml logs -f alertrix && rm test/data/homeserver.db

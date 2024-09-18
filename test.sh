921d04ed3450c26887d3ab9b17724237ca783c9e
Fabian Becker <info@alertrix.net>
2024-09-18 02:52:12 +0200

    move each command to its own line

--- test.sh
+++ test.sh
@@ -1,1 +1,10 @@
-sudo docker compose -f compose-test.yml build && sudo docker compose -f compose-test.yml down --volumes --remove-orphans && sudo docker compose -f compose-test.yml up -d && sudo docker compose exec nginx nginx -t && sudo docker compose -f compose-test.yml logs -f alertrix && rm test/data/homeserver.db
+sudo docker compose -f compose-test.yml build &&
+sudo docker compose -f compose-develop.yml down --volumes --remove-orphans &&
+sudo docker compose -f compose-test.yml down --volumes --remove-orphans &&
+rm -f data/test/synapse/homeserver.db &&
+sudo docker compose -f compose-test.yml up -d &&
+sudo docker compose exec nginx nginx -t &&
+sudo docker compose -f compose-test.yml logs -f alertrix &&
+sudo docker compose -f compose-test.yml down --volumes --remove-orphans &&
+rm data/test/synapse/homeserver.db &&
+echo \a

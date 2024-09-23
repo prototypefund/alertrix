09cf0a07c731737b2367e6f550776b8c166f23ba
Fabian Becker <info@alertrix.net>
2024-09-23 16:48:36 +0200

    automatically shut down the containers after running the tests. This line can be removed if you need to take a look at the log files

--- test.sh
+++ test.sh
@@ -7,4 +7,5 @@ sudo docker compose exec nginx nginx -t &&
 sudo docker compose -f compose-test.yml logs -f alertrix &&
 sudo docker compose -f compose-test.yml down --volumes --remove-orphans &&
 rm data/test/synapse/homeserver.db &&
+sudo docker compose -f compose-test.yml down &&
 echo \a
